93374fdce702b515657a81f34e62bd1dd2fc583a
Frklyn <86148912+Frklyn@users.noreply.github.com>
2025-05-06 16:50:44 +0200

    Add REp/components/Layout.js 1746543044267

--- REp/components/Layout.js
+++ REp/components/Layout.js
@@ -43,6 +43,12 @@ export default function Layout({ children }) {
                 >
                   Licenses
                 </Link>
+                <Link 
+                  href="/database"
+                  className="border-transparent text-gray-500 hover:border-primary-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                >
+                  Database
+                </Link>
               </div>
             </div>
 
